bee20795efc6d5d708803bb7bc2d7728e11ab20b
Zynkah <ffgurl07@yahoo.com>
2023-05-18 20:10:02 -0500

    user endpoint api

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,3 +1,12 @@
 // USERS CONTROLLERS FUNCTIONS
 
+const router = require("express").Router();
+const { getUser } = require("./database");
+
+router.route("/getUser").get((req, res, next) => {
+	query("firstName", "Missing parameter: firstName").exists(),
+		checkInput,
+		getUser;
+});
+
 module.exports = {};
