605f49ae8d00c9bf71e53546ada47804151f078f
serenkapanoglu <70541836+serenkapanoglu@users.noreply.github.com>
2023-06-02 09:30:15 -0400

    Update NavBar.js

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -4,7 +4,62 @@ import "./NavBar.css"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <!DOCTYPE html>
+<html>
+<head>
+  <title>Portfolio Site</title>
+  <!-- Include Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+<body>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="logo.png" alt="Logo" style="width: 70px; height: auto;">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#portfolio">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+ 
+  <div id="home">
+    <!-- Home section content -->
+  </div>
+
+  <div id="about">
+    <!-- About section content -->
+  </div>
+
+  <div id="portfolio">
+    <!-- Portfolio section content -->
+  </div>
+
+  <div id="contact">
+    <!-- Contact section content -->
+  </div>
+
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
   )
 }
 
