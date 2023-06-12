6ff3479c098821d0b70f7adffa6d541070d5a96c
Anthony R Barnett <89255759+barnettcoding@users.noreply.github.com>
2023-06-12 17:10:47 -0400

    made some minor adjustments

--- frontend/src/Components/AboutUs.js
+++ frontend/src/Components/AboutUs.js
@@ -1,12 +1,12 @@
 import React from "react";
 import CardsCarousel from "./CardsCarousel";
 
-import "./AboutUs.css";
+
 
 const AboutUs = () => {
    return (
       <div>
-         AboutUs
+      
          <CardsCarousel />
       </div>
    );
