7eaac93ed89e255dcc5ee75af4244faa1cdd2128
Anthony R Barnett <89255759+barnettcoding@users.noreply.github.com>
2023-06-17 05:42:24 -0400

    Adjustments made

--- frontend/src/Components/HeroSection.js
+++ frontend/src/Components/HeroSection.js
@@ -1,17 +1,17 @@
 import React from "react";
-import "./HeroSection.css";
-import skyImage from "../images/sky.jpg";
+import "./HeroSection.css"
+import robotHead from "../images/robotHead.webp"
 
 const HeroSection = () => {
   return (
     <div className="container-fluid header px-0 py-5">
       <div className="row g-0">
-        <div className="col-lg-6" style={{ position: 'relative' }}>
+        <div className="col-lg-6" style={{ position: 'absolute' }}>
           <div
             className="content-wrapper text-black p-5"
             style={{
               position: 'absolute',
-              top: '300px',
+              top: '600px',
               left: '50%',
               transform: 'translate(-50%, -50%)',
               backgroundColor: 'rgba(199, 186, 149, 0.4)', // Semi-transparent white backdrop
@@ -25,9 +25,9 @@ const HeroSection = () => {
             <p className="lead">
               Welcome to a community of passionate novice web developers who believe in the power of independence and self-driven experience. We are a group of dedicated individuals united by our common goal to build innovative projects while having fun along the way. In a world where experience is hard to come by, we take matters into our own hands and pave our own paths towards success.
             </p>
-            <p className="lead">
+            {/* <p className="lead">
               Join us and unlock the realm of endless possibilities, where creativity knows no bounds. Together, we embark on a journey of self-discovery and skill development, leveraging our collective knowledge and determination. From coding challenges to collaborative projects, we thrive in an environment that encourages exploration and experimentation.
-            </p>
+            </p> */}
             <div className="d-grid gap-2 d-md-flex justify-content-md-start">
               <button type="button" className="btn btn-outline-dark btn-lg px-4">
                 See Our Work
@@ -35,13 +35,13 @@ const HeroSection = () => {
             </div>
           </div>
         </div>
-        <div>
+        <div className="white-background">
           <img
-            src={skyImage}
+            src={robotHead}
             className="d-block w-100"
             alt="blue sky and mountain top"
             loading="lazy"
-            style={{ maxHeight: '800px' }}
+            style={{ maxHeight: '1200px', width: '50%', marginLeft: '400px'}}
           />
         </div>
       </div>
