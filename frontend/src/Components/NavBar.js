e98e5dc88c92261c6d64f1eca571e9b227622bed
unknown <sazwerling@gmail.com>
2023-06-22 15:57:54 -0700

    take out navbar.css, app.css, index.css

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,39 +1,57 @@
-import React from 'react'
-import "./NavBar.css"
-import logo from "../images/logo.png"
+import React from "react";
+import "./NavBar.css";
+import logo from "../images/logo.png";
 
 const NavBar = () => {
-  return (
-<>
-<nav className="navbar">
-      <div className="navbar-container">
-        <div className="logo-container">
-        <a href="#" className="navbar-logo">
-          <img src={logo} alt="Logo" className="navbar-logo-image" />
-        </a>
-        </div>
-        <ul className="navbar-links">
-        <li className="navbar-item">
-          <a className="nav-link" href="#home">Home</a>
-        </li>
-        <li className="navbar-item">
-          <a className="nav-link" href="#about">About Us</a>
-        </li>
-        <li className="navbar-item">
-          <a className="nav-link" href="#portfolio">Projects</a>
-        </li>
-        <li className="navbar-item">
-          <a className="nav-link" href="#contact">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+   return (
+      <>
+         <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+               <div class="container">
+                  <a class="navbar-brand" href="#">
+                     <img src={logo} alt="Logo" width="100" />
+                  </a>
+               </div>
 
- 
-  </>
+               <button
+                  class="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarNav"
+                  aria-controls="navbarNav"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+               >
+                  <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul className="navbar-nav">
+                     <li className="nav-item">
+                        <a className="nav-link active" href="#home">
+                           Home
+                        </a>
+                     </li>
+                     <li className="nav-item">
+                        <a className="nav-link" href="#about">
+                           About Us
+                        </a>
+                     </li>
+                     <li className="nav-item">
+                        <a className="nav-link" href="#portfolio">
+                           Projects
+                        </a>
+                     </li>
+                     <li className="nav-item">
+                        <a className="nav-link" href="#contact">
+                           Contact
+                        </a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </nav>
+      </>
+   );
+};
 
-
-  )
-}
-
-export default NavBar
+export default NavBar;
