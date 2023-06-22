b410c8c685df6477fd561ec06224345e7dfeb94e
unknown <sazwerling@gmail.com>
2023-06-22 15:08:32 -0700

    overlay positioned and responspive

--- frontend/src/Components/HeroSection.js
+++ frontend/src/Components/HeroSection.js
@@ -6,9 +6,9 @@ import robotHead from "../images/robotHead.webp"
 const HeroSection = () => {
   return (
     <div className="container-fluid header px-0">
-      <div className="row g-0 white-background" >
+      <div className="row g-0 background" >
         <div className="col-lg-6">
-          <div className="content-wrapper hero-info text-black p-4">
+          <div className="content-wrapper hero-info text-black p-5 lh-lg">
             <h1 className="display-5 fw-bold lh-1 mb-3">
               Building Our Dreams, Forging Our Paths
             </h1>
@@ -23,16 +23,14 @@ const HeroSection = () => {
             </div>
           </div>
         </div>
-        <div className="col-lg-6">
-          <div className="white-background">
+        <div className="col-lg-6 order-first order-lg-last">
             <img
               src={robotHead}
               className="d-block w-100"
-              alt="blue sky and mountain top"
+              alt="android skull with wiring"
               loading="lazy"
-              style={{ maxHeight: '1200px', width: '100%'}}
+              style={{ height: '100%'}}
             />
-          </div>
         </div>
       </div>
     </div>
