05a711ae8d5a5a0494700a55e452d5c4b156d0e2
Anthony R Barnett <89255759+barnettcoding@users.noreply.github.com>
2023-06-22 16:13:37 -0400

    footer fixes

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Footer.css'
+import logo from "../images/logo.png" 
+
 const Footer = () => {
   return (
     <>
@@ -7,27 +9,48 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <img src="logo.png" alt="Logo" style={{ width: '150px', height: 'auto' }} />
+            <img src={logo} alt="Logo" style={{ width: '150px', height: 'auto' }} />
           </div>
-          <div className="col-md-4">
-            <h5>Get In Touch</h5>
-            <form>
-              <div className="form-row">
-                <div className="col-md-8 form-group">
-                  <input type="email" className="form-control" placeholder="Enter your email address" />
-                </div>
-                <div className="col-md-4 form-group">
-                  <button type="submit" className="btn">Send</button>
-                </div>
+          <div className="col-md-4" style={{marginBottom: "20px"}}>
+  <h5>Get In Touch</h5>
+ 
+  <form>
+    <div className="form-row" style={{marginBottom: "20px"}}>
+      <div className="col-md-8 form-group" style={{marginBottom: "20px", marginLeft: "110px"}}>
+        <input type="email" className="form-control" placeholder="Enter your email address" />
+      </div>
+      <div className="col-md-4 form-group">
+        <button type="submit" className="btn" style={{marginLeft: "110px"}}>Send</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+<div className="col-md-4" >
+  <ul className="nobull">
+    <li>Members:</li>
+    <li>Anthony Barnett</li>
+    <li>David Palacios</li>
+    <li>Seth Zwerling</li>
+    <li>Peter Huynh</li>
+    <li>Seren Kapanoglu</li>
+    <li>Zena Creps</li>
+      
+       
+      
+      
+      </ul>
+      Copyright &copy; 2023 <span>Privacy Policy</span>
+    
+    </div>
+</div>
+
+<div className="col-md-4" style={{marginBottom: "20px"}}>
+ 
+</div>
 
     </div>
-    </form>
-    <div class="copyright text-center">
-      Copyright &copy; 2023 <span>Privacy Policy</span>
-    </div>
-    </div>
-    </div>
-    </div>
+   
     
 </footer>
 </>)
